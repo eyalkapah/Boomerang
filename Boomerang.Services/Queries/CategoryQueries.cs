fe419ba95e55a47f5c581039bfec9e56d6c768c3
Eyal Kapah <kaycee1@gmail.com>
2019-09-06 11:34:38 +0300

    improve the category repo

---
+++ Boomerang.Services/Queries/CategoryQueries.cs
@@ -0,0 +1,26 @@
+﻿using Boomerang.Models.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boomerang.Services.Queries
+{
+    public static class CategoryQueries
+    {
+        public static IQueryable<Category> QueryEnrollments(this IQueryable<Category> query)
+        {
+            query = query.Include(x => x.Sections).ThenInclude(x => x.Enrollments).ThenInclude(x => x.Packages).ThenInclude(x => x.Package).ThenInclude(x => x.Word);
+
+            query = query.Include(x => x.Sections).ThenInclude(x => x.Enrollments).ThenInclude(x => x.Packages).ThenInclude(x => x.Package).ThenInclude(x => x.ComplexWord);
+
+            return query;
+        }
+
+        public static IQueryable<Category> QuerySections(this IQueryable<Category> query) => query.Include(x => x.Sections).ThenInclude(x => x.Package).ThenInclude(x => x.Word);
+
+        public static IQueryable<Category> QuerySites(this IQueryable<Category> query) => query.Include(x => x.Sections).ThenInclude(x => x.Enrollments).ThenInclude(x => x.Site).ThenInclude(x => x.IrcInfo);
+    }
+}
--- Boomerang.Services/Repositories/CategoryRepository.cs
+++ Boomerang.Services/Repositories/CategoryRepository.cs
@@ -5,7 +5,9 @@ using Boomerang.Contracts.Interfaces;
 using Boomerang.Dtos;
 using Boomerang.Dtos.Resources;
 using Boomerang.Models.Models;
+using Boomerang.Services.Queries;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,30 +18,34 @@ namespace Boomerang.Services.Repositories
 {
     public class CategoryRepository : Repository<Category>, ICategoryRepository
     {
+        private readonly Dictionary<string, Action> _constants = new Dictionary<string, Action>();
         private readonly IMapper _mapper;
-
-        private Dictionary<string, string> _constants = new Dictionary<string, string>
-        {
-            { "sections", "Sections" }
-        };
+        private IQueryable<Category> _query;
 
         public CategoryRepository(ApplicationDbContext context, IMapper mapper) : base(context)
         {
             _mapper = mapper;
+
+            _query = Set.AsQueryable();
+
+            _constants.Add("sections", () => _query = _query.QuerySections());
+            _constants.Add("enrollments", () => _query = _query.QueryEnrollments());
+            _constants.Add("sites", () => _query = _query.QuerySites());
         }
 
         public IEnumerable<CategoryDto> GetCategories(CategoryResources resources)
         {
-            var query = Set.AsQueryable();
+            if (resources.Includes != null)
+            {
+                foreach (var item in resources.Includes?.Split(','))
+                {
+                    _constants.TryGetValue(item.ToLower(), out Action action);
 
-            var categories = query.Include(x => x.Sections).ThenInclude(x => x.Package)
-                .ThenInclude(x => x.Word)
-                .Include(x => x.Sections).ThenInclude(x => x.Package).ThenInclude(x => x.ComplexWord)
-                .Include(x => x.Sections).ThenInclude(x => x.Enrollments).ThenInclude(x => x.Site).ThenInclude(x => x.IrcInfo)
-                .Include(x => x.Sections).ThenInclude(x => x.Enrollments).ThenInclude(x => x.Packages).ThenInclude(x => x.Package).ThenInclude(x => x.Word)
-                .ToList();
+                    action?.Invoke();
+                }
+            }
 
-            var dto = _mapper.Map<IEnumerable<CategoryDto>>(categories);
+            var dto = _mapper.Map<IEnumerable<CategoryDto>>(_query);
 
             return dto;
         }
